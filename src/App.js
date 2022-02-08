cd9989722d4994eb5a578bcaa3d27f193a953984
DionSat <43707506+DionSat@users.noreply.github.com>
2022-02-08 11:43:50 -0800

    more edits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <h1 className="header">Hello React World!</h1>
+    <div style={{ margin: "10px 0", color: "purple", fontSize: "1.2em" }}>
+      {" "}
+      Millennium Falcon - YT-1300 light freighter
     </div>
+  </div>
   );
 }
 
