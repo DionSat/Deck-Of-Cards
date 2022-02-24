7b21d89ce31926fa048d46a0a0d2f56e90ad5e97
roikawa55 <61040017+roikawa55@users.noreply.github.com>
2022-02-23 18:49:11 -0800

    added navigation links to play and how-to-play .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
   Button,
   ButtonGroup,
-  Card,
   Container,
   Carousel,
   Form,
@@ -11,13 +10,14 @@ import {
   Navbar,
   Nav,
   Offcanvas,
+  Stack,
 } from "react-bootstrap";
 
 export default function Home() {
   return (
     <>
       <Navbar bg="light" expand={false}>
-        <Container fluid>
+        <Container fluid="lg">
           <Navbar.Brand>BlackJack</Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
           <Navbar.Offcanvas
@@ -33,25 +33,27 @@ export default function Home() {
             <Offcanvas.Body>
               <Nav className="justify-content-end">
                 <Nav.Link href="/">BlackJack</Nav.Link>
-                <Nav.Link>Other</Nav.Link>
+                <Nav.Link href="/play">Play</Nav.Link>
+                <Nav.Link href="/how-to-play">How to Play</Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
       </Navbar>
       <Container className="d-flex flex-column align-items-center justify-content-center h-100 bg-dark">
-      <Carousel variant="light">
-          <Carousel.Item>
-            <img className="d-block w-100"
-             src="https://images.unsplash.com/photo-1541278107931-e006523892df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
-             alt="the image of cards"
+        <Carousel variant="light">
+          <Carousel.Item className="d-block">
+            <img
+              className="d-flex"
+              src="https://images.unsplash.com/photo-1541278107931-e006523892df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+              alt="the image of cards"
             />
-          <Carousel.Caption >
-            <h1 id="carousel_caption">BlackJack</h1>
-          </Carousel.Caption>
+            <Carousel.Caption>
+              <h1 id="carousel_caption">BlackJack</h1>
+            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-        <br/>
+        <br />
         <Form>
           <Row className="mb-3">
             <Form.Group as={Col}>
@@ -74,7 +76,7 @@ export default function Home() {
             </Form.Group>
           </Row>
         </Form>
-        <br/>
+        <br />
         <ButtonGroup size="lg">
           <Button href="/play" variant="light">
             Play
