bfe6da1c82fe2efd85842baee8d30c9fad4c1aa3
roikawa55 <61040017+roikawa55@users.noreply.github.com>
2022-03-03 17:07:52 -0800

    styling about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -10,6 +10,9 @@ import {
 } from "react-bootstrap";
 
 export default function AboutUs() {
+  const card_back_image_src =
+    "https://cdn.pixabay.com/photo/2022/02/23/20/25/card-7031432_1280.png";
+  
   return (
     <Container
       fluid
@@ -43,33 +46,46 @@ export default function AboutUs() {
         </Container>
       </Navbar>
       <br />
-      <h1
-        id="jumbo_h1"
-        className="display-4 d-flex align-items-center justify-content-center bg-light"
-      >
-        About Us
-      </h1>
+      <h1 className="display-4 text-light">About Us</h1>
+      <h5 className="display-6 bg-light">We are a team of PSU students.</h5>
+      <p className="my-2 bg-light">
+        The goal if this project is providing accessible web-based BlackJack
+        Game.
+      </p>
+
       <br />
-      <Container className="d-flex flex-column align-items-center justify-content-center scrollbar">
-        <Row className="justify-content-space-around">
-          <Col>
+      <Container
+        fluid
+        className="d-flex flex-column align-items-center justify-content-center"
+      >
+        <Row>
+          <Col sm className="col-image">
+            <div className="card-inner">
+              <div className="card-front">
+                <img
+                  className="about-us-image"
+                  src={card_back_image_src}
+                  alt="the image of the backside of a card"
+                />
+              </div>
+              <div className="card-back">
+                <br/>
+                <p>Shane</p>
+                <p>PSU studnet</p>
+              </div>
+            </div>
+          </Col>
+          <Col sm className="col-image">
             <img
               className="about-us-image"
-              src="https://cdn.pixabay.com/photo/2022/02/23/20/25/card-7031432_1280.png"
+              src={card_back_image_src}
               alt="the image of the backside of a card"
             />
           </Col>
-          <Col>
+          <Col sm className="col-image">
             <img
               className="about-us-image"
-              src="https://cdn.pixabay.com/photo/2022/02/23/20/25/card-7031432_1280.png"
-              alt="the image of the backside of a card"
-            />
-          </Col>
-          <Col>
-            <img
-              className="about-us-image"
-              src="https://cdn.pixabay.com/photo/2022/02/23/20/25/card-7031432_1280.png"
+              src={card_back_image_src}
               alt="the image of the backside of a card"
             />
           </Col>
