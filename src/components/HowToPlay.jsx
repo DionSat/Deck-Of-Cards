1f02ba6e5126f67f78f8d6c750e91e66c5d67e32
roikawa55 <61040017+roikawa55@users.noreply.github.com>
2022-03-08 00:49:32 -0800

    add card value explanation to how-to-play

--- src/components/HowToPlay.jsx
+++ src/components/HowToPlay.jsx
@@ -27,19 +27,19 @@ export default function HowToPlay() {
             Beat the dealer by getting your hand total close to{" "}
             <span className="bg-danger text-white"> 21 </span> but going over{" "}
             <span className="bg-danger text-white"> 21 </span>.
-          </text>
+          </text>{" "}
           <text className="my-1 ">
             Start the game by press{" "}
             <span className="bg-success text-white"> Deal </span>. <br />
             If you'd like to draw another card to improve the count, press{" "}
             <span className="bg-air-force-blue text-light"> Hit </span>. <br />
             If you want to stick with your current hand, press{" "}
-            <span className="bg-air-force-blue text-light"> Stand </span>. <br />
+            <span className="bg-air-force-blue text-light"> Stand </span>.{" "}
+            <br />
             When your count is beneficial enough so you would like to double the
-            bet, press <span className="bg-air-force-blue text-light">
-              {" "}
-              Double{" "}
-            </span>. <br />
+            bet, press{" "}
+            <span className="bg-air-force-blue text-light"> Double </span>.{" "}
+            <br />
             It will give you exactly one more card and increase the initial bet
             by 100%. <br />
             When your initial cards are a pair of the same value and you would
@@ -57,6 +57,28 @@ export default function HowToPlay() {
           <text className="my-1 ">
             When you <span className="bg-warning text-dark"> lose </span> a
             game, you lose all your betting chips.
+          </text>
+          <h3 className="my-2">Card Values</h3>
+          <img
+            className="bg-white"
+            src="https://img.icons8.com/ios/50/000000/queen-of-clubs.png"
+            alt="image of queen of clubs"
+          />{" "}
+          <img
+            className="bg-white"
+            src="https://img.icons8.com/ios/50/000000/jack-of-spades.png"
+            alt="image of jack of spades"
+          />{" "}
+          <text className="my-3 ">Picture cards count as 10.</text>{" "}
+          <img
+            className="bg-white"
+            src="https://img.icons8.com/ios/50/000000/ace-of-clubs.png"
+            alt="image of ace of clubs"
+          />{" "}
+          <text className="my-3 ">The ace can count as either 1 or 11.</text>
+          <br />
+          <text className="my-1 ">
+            The rest count their face value. No meaning for card suits.
           </text>
           <br />
           <br />
@@ -89,3 +111,4 @@ export default function HowToPlay() {
     </>
   );
 }
+
