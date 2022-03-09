d5783f2747861bc5902308d54971cc446ba97112
DionSat <43707506+DionSat@users.noreply.github.com>
2022-03-08 17:22:04 -0800

    changed things back

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  HashRouter,
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import Play from "./Play.jsx";
 import HowToPlay from "./HowToPlay.jsx";
@@ -13,7 +8,7 @@ import CardValues from "./CardValue.jsx";
 
 export default function Routing() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/play' element={<Play />} />
