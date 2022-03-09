f73f326e4b7d9317a577013989c24a7ba240b41a
DionSat <43707506+DionSat@users.noreply.github.com>
2022-03-08 17:03:34 -0800

    small change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -136,7 +136,7 @@ export default function Home() {
           </Button>
           <Button
             id='rule-button'
-            href={process.env.PUBLIC_URL + "/how-to-play"}
+            href={"/how-to-play"}
             variant='outline-light'
             type='button'>
             Rules
